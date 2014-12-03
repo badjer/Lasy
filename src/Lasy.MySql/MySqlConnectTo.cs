7e7bc756f387e0ca196c437cbc517b9410f2aaaa
badjer <brian.dejong@gmail.com>
2014-12-03 08:34:14 -0800

    Moved src code; clean up nuget's silliness

---
+++ src/Lasy.MySql/MySqlConnectTo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lasy.MySql
+{
+    public static class MySqlConnectTo
+    {
+
+        public static SqlDB Db(string connString, bool strictTables = true)
+        {
+            return new MySqlDB(connString, new MySqlAnalyzer(connString), strictTables);
+        }
+
+        public static ModifiableSqlDB ModifiableDb(string connString, ITypedDBAnalyzer taxonomy = null)
+        {
+            var analyzer = new MySqlAnalyzer(connString);
+            var modifier = new MySqlModifier(connString, analyzer, taxonomy);
+            var db = new MySqlDB(connString, analyzer, false);
+            return new ModifiableSqlDB(db, modifier);
+        }
+    }
+}
